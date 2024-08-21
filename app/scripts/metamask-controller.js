2ff3b9f40905f176c4d57e6c3a4d84f2e1662291
digiwand <20778143+digiwand@users.noreply.github.com>
2024-08-21 16:47:09 -0400

    refactor: send sign metrics as event fragments in middleware

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -5435,9 +5435,6 @@ export default class MetamaskController extends EventEmitter {
 
     engine.push(
       createRPCMethodTrackingMiddleware({
-        trackEvent: this.metaMetricsController.trackEvent.bind(
-          this.metaMetricsController,
-        ),
         getMetricsState: this.metaMetricsController.store.getState.bind(
           this.metaMetricsController.store,
         ),
@@ -5453,6 +5450,7 @@ export default class MetamaskController extends EventEmitter {
           ],
         }),
         appStateController: this.appStateController,
+        metaMetricsController: this.metaMetricsController,
       }),
     );
 
